2c950dfb23740e643044cb78eb7ef432c0637cd2
lucasBruma <lucasbrumatti99@gmail.com>
2024-04-05 22:47:25 -0300

    get user location (latitude and longitude)

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,40 @@
+'use client'
 import Image from "next/image";
+import React, { useEffect } from 'react';
+
 
 export default function Home() {
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  const showPosition = (position) => {
+    console.log("Latitude: " + position.coords.latitude + 
+    " Longitude: " + position.coords.longitude);
+  }
+
+  const showError = (error) => {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        console.log("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.log("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        console.log("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        console.log("An unknown error occurred.");
+        break;
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
